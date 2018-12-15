f887688974098cb43e83a06ecf0c76cce36f20d4
unknown <2961132690@qq.com>
2018-12-15 22:09:29 +0800

    add some views

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,46 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    //文章列表
+    public function index()
+    {
+        return view('post/index');
+    }
+
+    //文章详情页面
+    public function show()
+    {
+        return view('post/show');
+    }
+
+    //创建文章
+    public function create()
+    {
+        return view('post/create');
+    }
+
+    //创建文章
+    public function store()
+    {
+
+    }
+
+    //编辑文章
+    public function edit()
+    {
+        return view('post/edit');
+
+    }
+
+    //编辑逻辑
+    public function update()
+    {
+
+    }
+
+    //删除
+    public function delete()
+    {
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,16 @@ Route::get('/', function () {
 });
 //控制器一定要带上命名空间
 //Route::get('/','[控制器]@[行为]');
+//文章列表页面
+Route::get('/posts','\App\Http\Controllers\PostController@index');
+//文章详情
+Route::get('/posts/{post}','\App\Http\Controllers\PostController@show');
+//创建文章
+Route::get('/posts/create','\App\Http\Controllers\PostController@create');
+Route::post('/posts','\App\Http\Controllers\PostController@store');
+//编辑文章
+
+Route::get('/posts/{posts}/edit','\App\Http\Controllers\PostController@edit');
+Route::put('/posts/{posts}','\App\Http\Controllers\PostController@update');
+//删除文章
+Route::get('/posts/delete','\App\Http\Controllers\PostController@delete');
